3d78324fadcffee3c33f3373ef164035343ad80e
parvez1407 <info.nahidwd@gmail.com>
2022-11-10 18:25:38 +0600

    Dianamically changed title name some corrections

--- src/AllServices/AllServices/AllServices.js
+++ src/AllServices/AllServices/AllServices.js
@@ -3,9 +3,11 @@ import Banner from '../../components/Home/Banner/Banner';
 import GetUpdate from '../../components/Home/GetUpdate/GetUpdate';
 import Growth from '../../components/Home/Growth/Growth';
 import TripTips from '../../components/Home/TripTips/TripTips';
+import useTitle from '../../hooks/useTitle';
 import ExploreAllServices from '../ExploreAllServices/ExploreAllServices';
 
 const AllServices = () => {
+    useTitle('AllServices')
     return (
         <div>
             <Banner></Banner>
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import useTitle from '../../../hooks/useTitle';
 import Banner from '../Banner/Banner';
 import GetUpdate from '../GetUpdate/GetUpdate';
 import Growth from '../Growth/Growth';
@@ -6,6 +7,7 @@ import Services from '../Services/Services';
 import TripTips from '../TripTips/TripTips';
 
 const Home = () => {
+    useTitle('Home')
     return (
         <div>
             <Banner></Banner>
